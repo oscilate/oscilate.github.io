baf49a64133953035df17ba6cedc8bfe22ff4909
sergiocosta <sergio.costa@highskillz.com>
2020-06-03 13:04:26 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+  var loading = $("#processing");
+  var go = $("#go");
+  var result = $("#result");
+  var baseurl = $("#baseurl");
+  loading.hide();
+  go.on("click", function (event) {
+    loading.show();
+    var apiurl = baseurl.val();
+    var fullUrl = apiurl + "/api/app/packagemanager/packages";
+    $.get(fullUrl)
+      .done(function (json) {
+        result.text(json.title).appendTo("body");
+      })
+      // Code to run if the request fails; the raw request and
+      // status codes are passed to the function
+      .fail(function (xhr, status, errorThrown) {
+        alert("Sorry, there was a problem!");
+        console.log("Error: " + errorThrown);
+        console.log("Status: " + status);
+        console.dir(xhr);
+      })
+      // Code to run regardless of success or failure;
+      .always(function (xhr, status) {
+        loading.hide();
+      });
+  });
+});
